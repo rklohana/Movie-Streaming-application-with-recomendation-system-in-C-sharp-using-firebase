4d8d4b124cb5a8ea112d8dabd583ff09dd7f40fb
rklohana <74131948+rklohana@users.noreply.github.com>
2020-12-18 22:11:42 +0500

    added file upload code

--- Form1.cs
+++ Form1.cs
@@ -10,6 +10,8 @@ using System.Windows.Forms;
 using FireSharp.Config;
 using FireSharp.Interfaces;
 using FireSharp.Response;
+using Firebase.Storage;
+using System.IO;
 namespace RIOFLIX123
 {
     public partial class Form1 : Form
@@ -21,10 +23,29 @@ namespace RIOFLIX123
 
         private async void Form1_Load(object sender, EventArgs e)
         {
-            Admin a=new Admin();
-            a.setdata("Ruhal","0 ","03421470676" ,"Misri street", "k190315@nu.edu.pk","Rklohana123");
-            SetResponse response = await a.client.SetAsync("Admin DATA/" + 2, a);
-            Admin result = response.ResultAs<Admin>();
+            
+            var stream = File.Open(@"C:\Users\rk\Pictures\Screenshots\isbah.png", FileMode.Open);
+
+            
+            var task = new FirebaseStorage("fir-fast-36fe8.appspot.com")
+                .Child("data")
+                .Child("project")
+                .Child("file.png")
+                .PutAsync(stream);
+           
+            
+            task.Progress.ProgressChanged += (s, rk) => MessageBox.Show($"Progress: {rk.Percentage} %");
+
+            
+            var downloadUrl = await task;
+
+
+            MessageBox.Show(downloadUrl);
+
+
+
+
+
         }
     }
 }
